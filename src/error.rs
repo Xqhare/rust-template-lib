137fb83ddeab73e7ec4f041e163c961cd50d8432
Xqhare <xqhare@web.de>
2026-04-05 04:54:45 +0200

    update(error): implement std::{error:Error, fmt::Display}

--- src/error.rs
+++ src/error.rs
@@ -1,9 +1,29 @@
+use std::fmt;
+
 pub type $NAMEResult<T> = Result<T, $NAMEError>;
 
 #[derive(Debug)]
 pub enum $NAMEError {
     Generic(String),
     Io(std::io::Error),
+}
+
+impl fmt::Display for $NAMEError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            $NAMEError::Generic(msg) => write!(f, "{}", msg),
+            $NAMEError::Io(err) => write!(f, "{}", err),
+        }
+    }
+}
+
+impl std::error::Error for $NAMEError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            $NAMEError::Generic(_) => None,
+            $NAMEError::Io(err) => Some(err),
+        }
+    }
 }
 
 impl From<std::io::Error> for $NAMEError {
