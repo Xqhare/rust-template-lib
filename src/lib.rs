c28f99745da3a0b62354ecaa3ffaf9376434feae
Xqhare <xqhare@web.de>
2026-04-06 03:27:56 +0200

    update clippy lints

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,27 @@
 #![warn(clippy::pedantic)]
 #![warn(clippy::all)]
 #![warn(clippy::restriction)]
-#![allow(
+#![expect(
     clippy::missing_docs_in_private_items,
     clippy::print_stdout,
-    clippy::implicit_return
+    clippy::implicit_return,
+    clippy::single_call_fn,
+    clippy::str_to_string,
+    clippy::question_mark_used,
+    clippy::indexing_slicing,
+    clippy::pattern_type_mismatch,
+    clippy::arbitrary_source_item_ordering,
+    clippy::doc_paragraphs_missing_punctuation,
+    clippy::exhaustive_enums,
+    clippy::min_ident_chars,
+    clippy::missing_trait_methods,
+    clippy::impl_trait_in_params,
+    clippy::as_conversions,
+    clippy::cast_lossless,
+    clippy::shadow_reuse,
+    clippy::blanket_clippy_restriction_lints,
+    clippy::doc_include_without_cfg,
+    reason = "Ignored warnings"
 )]
 
 mod error;
